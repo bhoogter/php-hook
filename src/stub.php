00656814a595b652ead428f652d2427f476af55a
BHOOGTER <bhoogter@ford.com>
2022-01-05 13:58:58 -0500

    Update tooling

--- src/stub.php
+++ src/stub.php
@@ -1,8 +1,7 @@
 <?php
 
 spl_autoload_register(function ($name) {
-    $d = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src");
-    if ($name == "php_hook") require_once($d . "/php-hook.php");
+    if ($name == "php_hook") require_once(__DIR__ . "/php-hook.php");
 });
 
 __HALT_COMPILER();
